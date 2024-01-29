0c75fc4eb211cd46d2c6fc9045b9caee89b1ea91
gusdn010158 <gusdn010158@gmail.com>
2024-01-29 23:53:01 +0900

    mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import Main from './Main';
+import Header from './Header';
+import Lounge from './Lounge';
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div>
-   dd
-    </div>
+    <>
+     <Router>
+      <Header/>
+    <Routes>
+<Route path="/*" element={<Main/>}></Route>
+<Route path="/Lounge" element={<Lounge/>}></Route>
+<Route path="/Login" element={<Login/>}></Route>
+<Route path="/Signup" element={<Signup/>}></Route>
+
+    </Routes>
+    </Router>
+    </>
   );
 }
 
 export default App;
+
+
