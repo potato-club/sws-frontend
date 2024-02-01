61d735606b57bdd17036d32d3aad89875bd7b22d
gusdn010158 <gusdn010158@gmail.com>
2024-02-01 19:48:16 +0900

    사이드바 오류 해결 및 마이페이지 하드 코딩

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+// App 컴포넌트
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './Main';
 import Header from './Header';
 import Lounge from './Lounge';
 import Login from './Login';
 import Signup from './Signup';
+import MyPage from './MyPage';
 
 function App() {
-  return (
-    <>
-     <Router>
-      <Header/>
-    <Routes>
-<Route path="/*" element={<Main/>}></Route>
-<Route path="/Lounge" element={<Lounge/>}></Route>
-<Route path="/Login" element={<Login/>}></Route>
-<Route path="/Signup" element={<Signup/>}></Route>
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);//사이드 박스 열기
 
-    </Routes>
-    </Router>
-    </>
-  );
+    const toggleSidebar = () => {// 클릭 이벤트에 의해 호출되어 사이드바의 열림/닫힘 상태를 토글
+        setIsSidebarOpen(prevState => !prevState);
+    };
+
+    return (
+        <>
+            <Router>
+                <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+                <Routes>
+                    <Route path="/*" element={<Main isSidebarOpen={isSidebarOpen} />} />  {/*ul업데이트 */}
+                    <Route path="/Lounge" element={<Lounge isSidebarOpen={isSidebarOpen} />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Signup" element={<Signup />} />
+                    <Route path="MyPage" element={<MyPage/>}/>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
+
+
+
 
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,46 +1,43 @@
-
-import React, { useState } from 'react';
+// Header 컴포넌트
+import React from 'react';
 import './Header.css';
-import { CgSearch } from "react-icons/cg";
 import { AiOutlineMenu } from "react-icons/ai";
-import {Link} from 'react-router-dom';
-function Header() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-   
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-       
-    };
+import { Link } from 'react-router-dom';
+import { CgSearch } from "react-icons/cg";
+interface HeaderProps {
+    toggleSidebar: () => void;
+    isSidebarOpen: boolean;
+}
 
+const Header: React.FC<HeaderProps> = ({ toggleSidebar, isSidebarOpen }) => {
     return (
         <div className="Header">
             <div className="Header_left">
                 <div className="MenuWrapper" onClick={toggleSidebar}>
-                    <AiOutlineMenu size="40"/>
+                    <AiOutlineMenu size="40" />
                     {isSidebarOpen && (
                         <div className="Sidebar">
-                            <Link to="/Lounge" style={{textDecoration:"none",color:"white"}}>라운지</Link>
-                        <div>설정</div>
-                        <hr></hr>
-                        <div>나</div>
-                        <hr></hr>
+                            <Link to="/Lounge" style={{ textDecoration: "none", color: "white",fontSize: "20px" }}>라운지</Link>
+                            <div>설정</div>
+                            <hr />
+                            <div>나</div>
+                            <hr />
                             <div>친구창</div>
-                            <div>마이페이지</div>
+                            <Link to="/MyPage" style={{ textDecoration: "none", color: "white",fontSize: "20px" }}>마이페이지</Link>
                             <div>사이드바 내용</div>
                         </div>
                     )}
                 </div>
-                <Link to="/" style={{textDecoration:"none",color:"white"}}>SWS</Link>
+                <Link to="/" style={{ textDecoration: "none", color: "white" }}>SWS</Link>
             </div>
-            
             <div className="Header_right">
                 <div className="Header_right_input">
-                    <input/>
-                    <div><CgSearch size="25"/></div>
+                    <input />
+                    <div><CgSearch size="25" /></div>
                 </div>
                 <div className="Header_right_login">
-                <Link to="/Login" style={{textDecoration:"none",color:"white"}}>로그인</Link>
-                <Link to="/Signup" style={{textDecoration:"none",color:"white"}}>회원가입</Link>
+                    <Link to="/Login" style={{ textDecoration: "none", color: "white" }}>로그인</Link>
+                    <Link to="/Signup" style={{ textDecoration: "none", color: "white" }}>회원가입</Link>
                 </div>
             </div>
         </div>
@@ -48,6 +45,10 @@ function Header() {
 }
 
 export default Header;
+
+
+
+
 
 
 
