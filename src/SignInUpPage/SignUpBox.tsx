6511b43d5c04ddc989c41ace6e837720ce621826
CHO YONGSU <choyongsu2001@naver.com>
2024-02-13 21:32:38 +0900

    Docs:회원가입 콘솔로 찍는 부분 수정 #9

--- src/SignInUpPage/SignUpBox.tsx
+++ src/SignInUpPage/SignUpBox.tsx
@@ -3,8 +3,8 @@
 import styled from "styled-components";
 import SignUpForm from "./SignUpForm";
 const SignUpBox=()=>{
-    const handleSignUpSubmit = (username: string, password: string, email:string) =>{
-        console.log('회원가입', username, password, email)
+    const handleSignUpSubmit = (username: string, password: string, email:string, nickname: string) =>{
+        console.log('회원가입', username, password, email, nickname)
     };
 
     return(
