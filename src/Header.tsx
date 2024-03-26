a7345e9d9d6b598a896b1fcbf6675f46d71e9730
gusdn010158 <gusdn010158@gmail.com>
2024-03-26 23:14:03 +0900

    Refactor lounge and change server #14 #19

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,242 +1,267 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 import { AiOutlineMenu } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { CgSearch } from "react-icons/cg";
-import { PRIMARY_COLOR_BLUE,PRIMARY_COLOR_SKY} from "../src/constants";
+import { PRIMARY_COLOR_BLUE, PRIMARY_COLOR_SKY } from "../src/constants";
 // @ts-ignore
 import logo from "../src/img/logo.png";
 
 interface HeaderProps {
-    toggleSidebar: () => void;
-    isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+  isSidebarOpen: boolean;
 }
 
 const HeaderContainer = styled.div`
-z-index: 9999; 
-    display: flex;
-    width: 100%;
-    height: 70px;
-    justify-content: space-between;
-    background-color:white;
-    color: black;
-    align-items: center;
-    border-left: 50px;
-    position: fixed;
-    border:1px solid black;
+  z-index: 9999;
+  display: flex;
+  width: 100%;
+  height: 70px;
+  justify-content: space-between;
+  background-color: white;
+  color: black;
+  align-items: center;
+  border-left: 50px;
+  position: fixed;
+  border: 1px solid black;
 `;
 
 const LeftContainer = styled.div`
-    display: flex;
-    width: 400px;
-    align-items: center;
-    justify-content: space-around;
+  display: flex;
+  width: 400px;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const RightContainer = styled.div`
-    display: flex;
-    width: 600px;
-    justify-content: space-around;
+  display: flex;
+  width: 600px;
+  justify-content: space-around;
 `;
 
 const SearchContainer = styled.div`
-    display: flex;
-    width: 280px;
-    align-items: center;
-    justify-content: space-around;
+  display: flex;
+  width: 280px;
+  align-items: center;
+  justify-content: space-around;
 `;
 const Searchinput = styled.input`
-    width: 220px;
-    border-right:0px solid black;
-    border-top:0px solid black;
-    border-left:0px solid black;
+  width: 220px;
+  border-right: 0px solid black;
+  border-top: 0px solid black;
+  border-left: 0px solid black;
 `;
 const Searchbutton = styled.button`
-   border:0px;
-   display:flex;
+  border: 0px;
+  display: flex;
 
-   background-color:white;
+  background-color: white;
 `;
 
-
-
 const LoginContainer = styled.div`
-    height: 25px;
-   
-    justify-content: space-around;
-    display: flex;
-    align-items: center;
+  height: 25px;
+
+  justify-content: space-around;
+  display: flex;
+  align-items: center;
 `;
 
 const Sidebar = styled.div`
-    top: 70px;
-    left: 0px;
-    position: fixed;
-    position: absolute;
-    height: 880px;
-    width: 200px;
-    background-color:white;
-    border:1px solid black;
+  top: 70px;
+  left: 0px;
+  position: fixed;
+  position: absolute;
+  height: 880px;
+  width: 200px;
+  background-color: white;
+  border: 1px solid black;
 `;
 
-
-const SidebarLink=styled(Link)`
-text-decoration:none;
-font-size:22px;
-display:flex;
-align-items:center;
-height:100%;
-border-radius:15px;
-padding-left:15px;
-color:black; 
-background-color: white;
-&.active {
-  background-color:${PRIMARY_COLOR_BLUE};
-  color:white; 
-}
-&:hover{
-  background-color: ${PRIMARY_COLOR_SKY};
-  color:white; 
-  transition: 1s;
-}
-
+const SidebarLink = styled(Link)`
+  text-decoration: none;
+  font-size: 22px;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  border-radius: 15px;
+  padding-left: 15px;
+  color: black;
+  background-color: white;
+  &.active {
+    background-color: ${PRIMARY_COLOR_BLUE};
+    color: white;
+  }
+  &:hover {
+    background-color: ${PRIMARY_COLOR_SKY};
+    color: white;
+    transition: 1s;
+  }
 `;
-const SidebarMe=styled.div`
-font-size:22px;
-height:100%;
-padding-left:15px;
-display:flex;
-align-items:center;
+const SidebarMe = styled.div`
+  font-size: 22px;
+  height: 100%;
+  padding-left: 15px;
+  display: flex;
+  align-items: center;
 `;
-const Sidebardiv=styled.div`
-height:55px;
-margin:15px;
-background-color: white;
-&.active {
- 
-  color:white; 
-}
+const Sidebardiv = styled.div`
+  height: 55px;
+  margin: 15px;
+  background-color: white;
+  &.active {
+    color: white;
+  }
 `;
-const Logo=styled.div`
+const Logo = styled.div`
   background: url(${logo});
   background-repeat: no-repeat;
-  width:120px;
-  height:50px;
-  background-position:center;
+  width: 120px;
+  height: 50px;
+  background-position: center;
   background-size: cover;
- 
 `;
-const HeadLink=styled(Link)`
-text-decoration:none;
-margin-right:30px;
-display:flex;
-align-items:center;
-height:50px;
-width:110px;
-border-radius:15px;
-justify-content:center;
-color:white; 
-background-color:${PRIMARY_COLOR_BLUE};
-
-
+const HeadLink = styled(Link)`
+  text-decoration: none;
+  margin-right: 30px;
+  display: flex;
+  align-items: center;
+  height: 50px;
+  width: 110px;
+  border-radius: 15px;
+  justify-content: center;
+  color: white;
+  background-color: ${PRIMARY_COLOR_BLUE};
 `;
-const MenuWrapper=styled.div`
-background-color:white;
-width:55px;
-height:50px;
-display:flex;
-border-radius:50%;
-justify-content:center;
-align-items:center;
-&:active {
-    background-color:${PRIMARY_COLOR_BLUE};
-}
+const MenuWrapper = styled.div`
+  background-color: white;
+  width: 55px;
+  height: 50px;
+  display: flex;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  &:active {
+    background-color: ${PRIMARY_COLOR_BLUE};
+  }
 `;
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar, isSidebarOpen }) => {
-    const [activeLink, setActiveLink] = useState<number | null>(null);
+  const [activeLink, setActiveLink] = useState<number | null>(null);
 
-    const handleClick = (index:number) => {
-      setActiveLink(index);
-    };
-    return (
-        <HeaderContainer>
-            <LeftContainer>
-            <MenuWrapper onClick={toggleSidebar}>
-                    <AiOutlineMenu size="40" />
-                    {isSidebarOpen && (
-                        <Sidebar>
-                            <Sidebardiv  className={`SidebarLink ${activeLink === 0 ? "active" : ""}`} 
-                             onClick={() => handleClick(0)}>
-                            <SidebarLink to="/Lounge" 
-                            className={`SidebarLink ${activeLink === 0 ? "active" : ""}`} 
-                            onClick={() => handleClick(0)}
-                            >라운지</SidebarLink>
-                            </Sidebardiv>
+  const handleClick = (index: number) => {
+    setActiveLink(index);
+  };
+  return (
+    <HeaderContainer>
+      <LeftContainer>
+        <MenuWrapper onClick={toggleSidebar}>
+          <AiOutlineMenu size="40" />
+          {isSidebarOpen && (
+            <Sidebar>
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 0 ? "active" : ""}`}
+                onClick={() => handleClick(0)}
+              >
+                <SidebarLink
+                  to="/Lounge"
+                  className={`SidebarLink ${activeLink === 0 ? "active" : ""}`}
+                  onClick={() => handleClick(0)}
+                >
+                  라운지
+                </SidebarLink>
+              </Sidebardiv>
 
-                            <Sidebardiv  className={`SidebarLink ${activeLink === 1 ? "active" : ""}`} 
-                             onClick={() => handleClick(1)}>
-                            <SidebarLink to="/" 
-                            className={`SidebarLink ${activeLink === 1 ? "active" : ""}`} 
-                            onClick={() => handleClick(1)}
-                            >설정</SidebarLink>
-                            </Sidebardiv>
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 1 ? "active" : ""}`}
+                onClick={() => handleClick(1)}
+              >
+                <SidebarLink
+                  to="/"
+                  className={`SidebarLink ${activeLink === 1 ? "active" : ""}`}
+                  onClick={() => handleClick(1)}
+                >
+                  설정
+                </SidebarLink>
+              </Sidebardiv>
 
-                            <Sidebardiv className={`SidebarLink ${activeLink === 2 ? "active" : ""}`} 
-                             onClick={() => handleClick(2)}>
-                            <SidebarLink to="/Make"  className={`SidebarLink ${activeLink === 2 ? "active" : ""}`} 
-        onClick={() => handleClick(2)}>팀 만들기</SidebarLink>
-                            </Sidebardiv>
-                            <hr />
-                            <Sidebardiv  className={`SidebarLink ${activeLink === 3 ? "active" : ""}`} 
-                             onClick={() => handleClick(3)}>
-                            <SidebarMe>나</SidebarMe>
-                            </Sidebardiv>
-                            <hr />
-                            <Sidebardiv  className={`SidebarLink ${activeLink === 4 ? "active" : ""}`} 
-                             onClick={() => handleClick(4)}>
-                            <SidebarLink to="/"  className={`SidebarLink ${activeLink === 4 ? "active" : ""}`} 
-        onClick={() => handleClick(4)}>친구창</SidebarLink>
-                            </Sidebardiv>
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 2 ? "active" : ""}`}
+                onClick={() => handleClick(2)}
+              >
+                <SidebarLink
+                  to="/Make"
+                  className={`SidebarLink ${activeLink === 2 ? "active" : ""}`}
+                  onClick={() => handleClick(2)}
+                >
+                  팀 만들기
+                </SidebarLink>
+              </Sidebardiv>
+              <hr />
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 3 ? "active" : ""}`}
+                onClick={() => handleClick(3)}
+              >
+                <SidebarMe>나</SidebarMe>
+              </Sidebardiv>
+              <hr />
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 4 ? "active" : ""}`}
+                onClick={() => handleClick(4)}
+              >
+                <SidebarLink
+                  to="/"
+                  className={`SidebarLink ${activeLink === 4 ? "active" : ""}`}
+                  onClick={() => handleClick(4)}
+                >
+                  친구창
+                </SidebarLink>
+              </Sidebardiv>
 
-                            <Sidebardiv className={`SidebarLink ${activeLink === 5 ? "active" : ""}`} 
-                             onClick={() => handleClick(5)}>
-                            <SidebarLink to="/MyPage"  className={`SidebarLink ${activeLink === 5 ? "active" : ""}`} 
-        onClick={() => handleClick(5)}>마이 페이지</SidebarLink>
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 5 ? "active" : ""}`}
+                onClick={() => handleClick(5)}
+              >
+                <SidebarLink
+                  to="/MyPage"
+                  className={`SidebarLink ${activeLink === 5 ? "active" : ""}`}
+                  onClick={() => handleClick(5)}
+                >
+                  마이 페이지
+                </SidebarLink>
+              </Sidebardiv>
+              <Sidebardiv
+                className={`SidebarLink ${activeLink === 6 ? "active" : ""}`}
+                onClick={() => handleClick(6)}
+              >
+                <SidebarLink
+                  to="/"
+                  className={`SidebarLink ${activeLink === 6 ? "active" : ""}`}
+                  onClick={() => handleClick(6)}
+                >
+                  사이드바 내용
+                </SidebarLink>
+              </Sidebardiv>
+            </Sidebar>
+          )}
+        </MenuWrapper>
 
-</Sidebardiv>
-<Sidebardiv  className={`SidebarLink ${activeLink === 6 ? "active" : ""}`} 
-                             onClick={() => handleClick(6)}>
-                            <SidebarLink to="/"  className={`SidebarLink ${activeLink === 6 ? "active" : ""}`} 
-        onClick={() => handleClick(6)}>사이드바 내용</SidebarLink>
-                            </Sidebardiv>
-                        </Sidebar>
-                    )}
-               </MenuWrapper>
-
-                <Link to="/"><Logo></Logo></Link>
-            </LeftContainer>
-            <RightContainer>
-                <SearchContainer>
-                    <Searchinput placeholder='해시 태크를 검색해 보세요'/>
-                    <Searchbutton><CgSearch size="25" /></Searchbutton>
-
-                </SearchContainer>
-                <LoginContainer>
-                    <HeadLink to="/SignInUpPage/SignInUpBox">Sign In/Up</HeadLink>
-                </LoginContainer>
-            </RightContainer>
-        </HeaderContainer>
-
-    );
-}
+        <Link to="/">
+          <Logo></Logo>
+        </Link>
+      </LeftContainer>
+      <RightContainer>
+        <SearchContainer>
+          <Searchinput placeholder="해시 태크를 검색해 보세요" />
+          <Searchbutton>
+            <CgSearch size="25" />
+          </Searchbutton>
+        </SearchContainer>
+        <LoginContainer>
+          <HeadLink to="/SignInUpPage/SignInUpBox">Sign In/Up</HeadLink>
+        </LoginContainer>
+      </RightContainer>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
-
-
-
-
-
-
-
-
--- src/Loungesecond.tsx
+++ src/Loungesecond.tsx
@@ -1,26 +1,27 @@
-import React, { useState, ChangeEvent } from 'react';
-import styled from 'styled-components';
+import React, { useState, ChangeEvent } from "react";
+import styled from "styled-components";
 
- const BigBox = styled.div`
+const BigBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
- 
 `;
 
 const BigBoxWrite = styled.p`
   position: absolute;
   left: 50%;
-  transform: translateX(-50%); //translateX를 사용함으로서 위치 결정에 영향을 주지 않고 수평으로 중심을 잡아준다.
+  transform: translateX(
+    -50%
+  ); //translateX를 사용함으로서 위치 결정에 영향을 주지 않고 수평으로 중심을 잡아준다.
   display: flex;
   justify-content: center;
   padding-bottom: 750px;
   padding-left: 110px;
   font-size: 30px;
   color: #7ba1da;
-`
+`;
 
- const Box = styled.div`
+const Box = styled.div`
   width: 1600px;
   height: 850px;
   padding-top: 40px;
@@ -31,7 +32,7 @@ const BigBoxWrite = styled.p`
   background-color: white;
 `;
 
- const InnerBox = styled.div`
+const InnerBox = styled.div`
   width: 1000px;
   height: 800px;
   display: flex;
@@ -41,50 +42,47 @@ const BigBoxWrite = styled.p`
   background-color: white;
   //box-shadow: 3px 3px 3px 3px gray;
 `;
- const SmallBox = styled.div`
+const SmallBox = styled.div`
   width: 100%;
   height: 50%;
   border-radius: 20px;
   display: flex;
   flex-direction: row;
   border-bottom: 3px solid #7ba1da;
-  background-color:#7ba1da; 
+  background-color: #7ba1da;
   box-shadow: 2px 2px 2px 2px gray;
 `;
 
 const RowBox1 = styled.div`
   width: 30%;
   height: 100%;
-  background-color:#7ba1da; 
+  background-color: #7ba1da;
   display: flex;
   flex-direction: column;
   border-right: 3px solid #7ba1da;
   border-radius: 20px;
-
 `;
 
 const RowBox2 = styled.div`
   width: 70%;
   height: 100%;
-  background-color:#7ba1da;
+  background-color: #7ba1da;
   display: flex;
   flex-direction: column;
   border-radius: 20px;
 `;
 
- const  RowSmallBox1 = styled.div`
+const RowSmallBox1 = styled.div`
   width: 100%;
   height: 30%;
- 
 `;
 
- const  RowSmallBox2 = styled.div`
+const RowSmallBox2 = styled.div`
   width: 100%;
   height: 70%;
- 
 `;
 
-const  RowSmallBox3 = styled.div`
+const RowSmallBox3 = styled.div`
   width: 100%;
   height: 30%;
   display: flex;
@@ -92,49 +90,51 @@ const  RowSmallBox3 = styled.div`
   justify-content: space-between; /* Added space between button and counter */
 `;
 
- const InputFieldA = styled.textarea`
+const InputFieldA = styled.textarea`
   width: 95%;
   height: 30%;
   margin-left: 3%;
   padding: 5px;
-  border: 3px solid #A4A4A4;
+  border: 3px solid #a4a4a4;
   font-size: 20px;
   text-align: left;
   box-sizing: border-box;
   resize: none; // Prevent textarea from being resized by user
 
-  &:hover { //hover를 넘으로써 애니메이션 적용
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
-        transform: scale(1.0);
-    }
+  &:hover {
+    //hover를 넘으로써 애니메이션 적용
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+    transform: scale(1);
+  }
 `;
 
- const InputFieldB = styled.textarea`
+const InputFieldB = styled.textarea`
   width: 95%;
   height: 100%;
   margin-left: 3%;
   margin-top: 2%;
   padding: 5px;
-  border: 3px solid #A4A4A4;
+  border: 3px solid #a4a4a4;
   font-size: 20px;
   text-align: left;
   box-sizing: border-box;
   resize: none; // Prevent textarea from being resized by user
 
-  &:hover { //hover를 넘으로써 애니메이션 적용
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
-        transform: scale(1.0);
-    }
+  &:hover {
+    //hover를 넘으로써 애니메이션 적용
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+    transform: scale(1);
+  }
 `;
 
- const RowSmallBox4 = styled.div`
+const RowSmallBox4 = styled.div`
   width: 100%;
   height: 70%;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
- const  MainWrite = styled.div`
+const MainWrite = styled.div`
   font-size: 20px;
   align-items: center;
   justify-content: center;
@@ -143,10 +143,10 @@ const  RowSmallBox3 = styled.div`
   height: 100%;
   border: 1px solid white;
   border-radius: 10px;
-  color: #ffff 
+  color: #ffff;
 `;
 
- const InnerWhiteBox = styled.div`
+const InnerWhiteBox = styled.div`
   position: relative;
   width: 95%;
   height: 95%;
@@ -159,14 +159,13 @@ const  RowSmallBox3 = styled.div`
   overflow: hidden;
   object-fit: cover;
 
-  &:hover{
+  &:hover {
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
-    transform: scale(1.0);
+    transform: scale(1);
   }
- 
 `;
 // Adding a new styled component for the image
- const Image = styled.img`
+const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
@@ -231,37 +230,37 @@ const CompletionBox = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate()(-50%, -50%);
+  transform: translate() (-50%, -50%);
   background-color: #ffffff;
   padding: 10px;
   border: 1px solid #ffffff;
-`
+`;
 const CompletionButton = styled.button`
   font-size: 15px;
   padding: 5px 10px;
   background-color: #7ba1da;
   border-radius: 10px;
 
-  &:hover { //hover를 넘으로써 애니메이션 적용
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
-        transform: scale(1.0);
-    }
-`
+  &:hover {
+    //hover를 넘으로써 애니메이션 적용
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+    transform: scale(1);
+  }
+`;
 const CompletionButtonContainer = styled.div`
   margin-top: auto;
   margin-left: 1550px;
   align-self: flex-start;
-`
+`;
 
 function SecondPage() {
   const [likes, setLikes] = useState(0);
-  const [inputBValue, setInputBValue] = useState('');
+  const [inputBValue, setInputBValue] = useState("");
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const imagesLinkes = [
-    'https://ichef.bbci.co.uk/ace/ws/800/cpsprodpb/E172/production/_126241775_getty_cats.png',
-    'https://flexible.img.hani.co.kr/flexible/normal/970/777/imgdb/resize/2019/0926/00501881_20190926.JPG'
-  
+    "https://ichef.bbci.co.uk/ace/ws/800/cpsprodpb/E172/production/_126241775_getty_cats.png",
+    "https://flexible.img.hani.co.kr/flexible/normal/970/777/imgdb/resize/2019/0926/00501881_20190926.JPG",
   ];
 
   const handleLike = () => {
@@ -272,92 +271,107 @@ function SecondPage() {
 
   const handleCompletion = () => {
     setIsCompleted(true);
-    setTimeout(()=> {
+    setTimeout(() => {
       setIsCompleted(false);
     }, 3500);
-  }
+  };
 
-  const handleInputBChange = (e: ChangeEvent<HTMLTextAreaElement>) => { // ChangeEvent<HTMLTextAreaElement> instead of ChangeEvent<HTMLInputElement>
+  const handleInputBChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    // ChangeEvent<HTMLTextAreaElement> instead of ChangeEvent<HTMLInputElement>
     const value = e.target.value;
     setInputBValue(value);
   };
 
   const handleLeftArrowClick = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imagesLinkes.length - 1 : prevIndex - 1));
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? imagesLinkes.length - 1 : prevIndex - 1
+    );
   };
 
   const handleRightArrowClick = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === imagesLinkes.length - 1 ? 0 : prevIndex + 1));
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === imagesLinkes.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   return (
     <div>
-     <BigBox>
-      <BigBoxWrite>
-        글작성
-      </BigBoxWrite>
-      <Box>
-        <InnerBox>
-          <SmallBox>
-            <RowBox1>
-              <RowSmallBox1>
-                <MainWrite>제목</MainWrite>
-              </RowSmallBox1>
-              <RowSmallBox2>
-                <MainWrite>사진</MainWrite>
-              </RowSmallBox2>
-            </RowBox1>
-            
-            <RowBox2>
-              <RowSmallBox3>
-                <InputFieldA rows={1} maxLength={20} /> 
-              </RowSmallBox3>
-              <RowSmallBox4>
-        <InnerWhiteBox>
-          <Image src={imagesLinkes[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
+      <BigBox>
+        <BigBoxWrite>글작성</BigBoxWrite>
+        <Box>
+          <InnerBox>
+            <SmallBox>
+              <RowBox1>
+                <RowSmallBox1>
+                  <MainWrite>제목</MainWrite>
+                </RowSmallBox1>
+                <RowSmallBox2>
+                  <MainWrite>사진</MainWrite>
+                </RowSmallBox2>
+              </RowBox1>
 
-          {/* Left Arrow */}
-          <LeftArrow onClick={handleLeftArrowClick}>{'<'}</LeftArrow>
+              <RowBox2>
+                <RowSmallBox3>
+                  <InputFieldA rows={1} maxLength={20} />
+                </RowSmallBox3>
+                <RowSmallBox4>
+                  <InnerWhiteBox>
+                    <Image
+                      src={imagesLinkes[currentImageIndex]}
+                      alt={`Image ${currentImageIndex + 1}`}
+                    />
 
-          {/* Right Arrow */}
-          <RightArrow onClick={handleRightArrowClick}>{'>'}</RightArrow>
+                    {/* Left Arrow */}
+                    <LeftArrow onClick={handleLeftArrowClick}>{"<"}</LeftArrow>
 
-          {/* Dots */}
-          <DotsContainer>
-            {imagesLinkes.map((_, index) => (
-              <Dot
-                key={index}
-                style={{ backgroundColor: index === currentImageIndex ? '#555' : '#bbb' }}
-              />
-            ))}
-          </DotsContainer>
-        </InnerWhiteBox>
-      </RowSmallBox4>
-            </RowBox2>
-          </SmallBox>
+                    {/* Right Arrow */}
+                    <RightArrow onClick={handleRightArrowClick}>
+                      {">"}
+                    </RightArrow>
 
-          <SmallBox>
-            <RowBox1>
-              <MainWrite>내용</MainWrite>
-            </RowBox1>
-            <RowBox2>
-              <InputFieldB rows={3} value={inputBValue} onChange={handleInputBChange} maxLength={500} />
-              <p>({inputBValue.length} / 500 )</p>
+                    {/* Dots */}
+                    <DotsContainer>
+                      {imagesLinkes.map((_, index) => (
+                        <Dot
+                          key={index}
+                          style={{
+                            backgroundColor:
+                              index === currentImageIndex ? "#555" : "#bbb",
+                          }}
+                        />
+                      ))}
+                    </DotsContainer>
+                  </InnerWhiteBox>
+                </RowSmallBox4>
+              </RowBox2>
+            </SmallBox>
 
-              {isCompleted && (
-            <CompletionBox>
-              <p>작성완료가 되었습니다.</p>
-            </CompletionBox> 
-          )}
-              
-            </RowBox2>
-          </SmallBox>
-        </InnerBox>
-      </Box>
-    </BigBox>
-    <CompletionButtonContainer>
+            <SmallBox>
+              <RowBox1>
+                <MainWrite>내용</MainWrite>
+              </RowBox1>
+              <RowBox2>
+                <InputFieldB
+                  rows={3}
+                  value={inputBValue}
+                  onChange={handleInputBChange}
+                  maxLength={500}
+                />
+                <p>({inputBValue.length} / 500 )</p>
+
+                {isCompleted && (
+                  <CompletionBox>
+                    <p>작성완료가 되었습니다.</p>
+                  </CompletionBox>
+                )}
+              </RowBox2>
+            </SmallBox>
+          </InnerBox>
+        </Box>
+      </BigBox>
+      <CompletionButtonContainer>
         <CompletionButton onClick={handleCompletion}>작성완료</CompletionButton>
-      </CompletionButtonContainer>  
+      </CompletionButtonContainer>
     </div>
   );
 }
